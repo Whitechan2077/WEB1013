3c9602f71a6cf1ff02842e2cda226949824d8d48
Hoàng Dũng <dungbui8198@gmail.com>
2023-05-23 17:33:02 +0700

    html css
    nah

---
+++ asm rebuild/main.js
@@ -0,0 +1,14 @@
+var a = {
+    name:'Dit me may',
+    age:'lon',
+    ad:'chuong cho dit con me tuoi lon',
+    lmao:123,
+}
+var b = [
+    1,2,'lmao','dit'
+]
+var d = 1;
+var c = '1'
+console.log(c!==d)
+var f=d.toString;  
+console.log(c);
